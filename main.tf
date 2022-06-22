1fe27a644507b763f2ca9d3b844266a191c2e507
Savita Pal <savita.pal08@gmail.com>
2022-06-22 15:56:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_dataflow_job" "big_data_job" {
   project      = var.project_id
   kms_key_name = "projects/modular-scout-345114/locations/global/keyRings/keyring-example/cryptoKeys/crypto-key-example"
   service_account_email = "composer-test-sa2@modular-scout-345114.iam.gserviceaccount.com"
-  ip_configuration = "WORKER_IP_PUBLIC" # "WORKER_IP_PRIVATE"
+  ip_configuration = null # "WORKER_IP_PUBLIC" # "WORKER_IP_PRIVATE"
   template_gcs_path = "gs://my-bucket-df/templates/template_file/latest_Word_Count"
   temp_gcs_location = "gs://my-bucket-df/tmp_dir"
   
