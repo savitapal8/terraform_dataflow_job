6e4703224b1039a47a4ad6ac4a96a2974989a8ad
Savita Pal <savita.pal08@gmail.com>
2022-10-20 13:57:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 resource "google_dataflow_job" "big_data_job" {
   name         = var.df-name
   project      = var.project_id
-  #kms_key_name = "projects/modular-scout-345114/locations/global/keyRings/keyring-example/cryptoKeys/crypto-key-example"
+  kms_key_name = "projects/modular-scout-345114/locations/global/keyRings/keyring-example/cryptoKeys/crypto-key-example"
   service_account_email = "composer-test-sa2@modular-scout-345114.iam.gserviceaccount.com"  # null # "compute@developer.gserviceaccount.com"
   ip_configuration = "WORKER_IP_PRIVATE" # null # "WORKER_IP_PUBLIC"
   template_gcs_path = "gs://my-bucket-df/templates/template_file/latest_Word_Count"
   temp_gcs_location = "gs://my-bucket-df/tmp_dir"
   
-  labels = {
-    gcp_region = "us",
-      owner = "hybridenv",
-      application_division = "pci",
-      application_name = "demo",
-      application_role = "app",
-      environment = "prod",
-      au = "0223092",
-      created = "20220802",
-  }
+  #labels = {
+  #  gcp_region = "us",
+  #    owner = "hybridenv",
+  #    application_division = "pci",
+  #    application_name = "demo",
+  #    application_role = "app",
+  #    environment = "prod",
+  #    au = "0223092",
+  #    created = "20220802",
+  #}
   
 }
